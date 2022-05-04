90697cb85aa9a248fd11cd23a4d1700996e56974
Demix88 <skrasnauskis@gmail.com>
2022-05-04 07:59:55 +0100

    few minor changes

--- JavaScript/Clock.js
+++ JavaScript/Clock.js
@@ -1,10 +1,14 @@
 setInterval(setClock,1000)
 
+
 const hourHand=document.querySelector('[data-hour-hand')
 const minuteHand=document.querySelector('[data-minute-hand')
 const secondHand=document.querySelector('[data-second-hand')
 
+
 function setClock() {
+    
+    
     const  currentDate=new Date()
     const secondsRatio=currentDate.getSeconds()/60
     const minutesRatio=(secondsRatio +currentDate.getMinutes())/60
@@ -15,6 +19,10 @@ function setClock() {
 }
 
 function setRotation(element,rotationRatio){
+   
+   
     element.style.setProperty('--rotation',rotationRatio*360)
+    
+    
 }
 setClock()
